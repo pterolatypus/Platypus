382fc6ac5cd7755456a8c3f1fe7f1e3f7adaa651
Pterolatypus <pterolatypus@gmail.com>
2015-09-22 09:38:08 +0100

    Code base

---
+++ src/main/java/common/reference/Reference.java
@@ -0,0 +1,19 @@
+/*
+ * Platypus is a modification for Minecraft, built against Minecraft Forge. Copyright (C) 2015 Pterolatypus
+ * This program is free software: you can redistribute it and/or modify it under the terms of version 2 of the GNU General Public License as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+
+package common.reference;
+
+
+public class Reference {
+
+    public static final String MOD_ID = "@MOD_ID@";
+    public static final String MOD_NAME = "@MOD_NAME@";
+    public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String PROXY_CLIENT = "com.github.pterolatypus.rift.proxy.ClientProxy";
+    public static final String PROXY_SERVER = "com.github.pterolatypus.rift.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "com.github.pterolatypus.rift.client.gui.GuiFactory";
+
+}
